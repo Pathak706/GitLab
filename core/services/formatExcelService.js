7bd782c3d02ca54a9ad3bd21b78932f65c03f0c0
Vinit Dholakia <vinit.dholakia@checkapp.in>
2019-01-30 10:28:31 -0800

    Excel Output

--- core/services/formatExcelService.js
+++ core/services/formatExcelService.js
@@ -1,27 +1,34 @@
 var json2xls = require('json2xls');
 
-function writeExcel() {
-    require('fs').writeFile('data.xlsx', xls, 'binary',(err)=>{
-
+function writeExcel(_id, _type, _data) {
+    return new Promise(function(resolve, reject) {
+        let fn = _id + '_' + _type.split(" ").join("") + '.xlsx'
+        require('fs').writeFile(process.cwd() + '/../xlsx/' + fn, _data, 'binary', (err) => {
+            if (err) {
+                return reject(err);
+            } else {
+                resolve("v1/projects/xlsx/" + fn);
+                return;
+            }
+        });
     });
+
 }
 
 function formatExcel(projects) {
-    console.log(JSON.stringify(projects, null, 2))
+    console.log(JSON.stringify(projects, null, 2));
     return new Promise(async function(resolve, reject) {
         for (var i = 0; i < projects.length; i++) {
             let projectId = projects[i].projectId || null;
             let expenses = projects[i].expenses || [];
             for (var j = 0; j < expenses.length; j++) {
                 var xls = json2xls(expenses[j].data);
-
-                let filePath = await writeExcel(expenses[j].type, xls);
-
-                expenses[j].data
+                let filePath = await writeExcel(projectId, expenses[j].type, xls);
+                projects[i].excelData = projects[i].excelData || {};
+                projects[i].excelData[expenses[j].type] = filePath;
             }
-
         }
-        resolve("<b> Data is Ready </b>");
+        resolve(projects[0].excelData);
     });
 }
 module.exports = formatExcel;
